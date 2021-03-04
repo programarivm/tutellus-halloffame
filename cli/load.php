2770236b451cecbb7ae194caf8283393797e4995
programarivm <info@programarivm.com>
2021-03-04 17:47:47 +0100

    Implemented TutellusHall\Fame

--- cli/load.php
+++ cli/load.php
@@ -2,11 +2,9 @@
 
 require __DIR__.'/../bootstrap.php';
 
-use TutellusHall\Fame;
-
 $query = json_decode(file_get_contents(__DIR__.'/../query/web.js'));
 $layout = file_get_contents(__DIR__.'/../layout/grav/quark.html');
 
-$html = (new Fame($query, $layout))->html();
+$html = (new TutellusHall\Fame($query, $layout))->html();
 
 print_r($html);
--- src/Fame.php
+++ src/Fame.php
@@ -4,6 +4,12 @@ namespace TutellusHall;
 
 class Fame
 {
+    const PARAM_CURRENCY    = 'EUR';
+    const PARAM_PRICE_MIN   = 5;
+    const PARAM_PRICE_MAX   = 95;
+    const PARAM_SORT        = 'publish_date';
+    const PARAM_LIMIT       = 7;
+
     private $query;
 
     private $layout;
@@ -14,8 +20,53 @@ class Fame
         $this->layout = $layout;
     }
 
+    private function replace(array $item)
+    {
+        $url = str_replace('affref=undefined', 'affref='.$_ENV['AFF_REF'], $item['url']);
+        $replaced = str_replace('/name/', $item['name'], $this->layout);
+        $replaced = str_replace('/image_url/', $item['image_url'], $replaced);
+        $replaced = str_replace('/summary/', $item['summary'], $replaced);
+        $replaced = str_replace('/url/', $url, $replaced);
+        $replaced = str_replace('/EUR/', $item['price']['EUR'], $replaced);
+
+        return $replaced;
+    }
+
     public function html()
     {
-        return 'TODO';
+        $arr = [];
+        foreach ($this->query as $query) {
+            $url = $_ENV['API_URL'].
+                "/courses?category_code={$query->category_code}&subcategory_code={$query->subcategory_code}&q={$query->q}&currency="
+                .self::PARAM_CURRENCY."&price_min="
+                .self::PARAM_PRICE_MIN."&price_max="
+                .self::PARAM_PRICE_MAX."&sort="
+                .self::PARAM_SORT."&limit="
+                .self::PARAM_LIMIT;
+
+            $ch = curl_init();
+
+            curl_setopt($ch, CURLOPT_URL, $url );
+            curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1 );
+            curl_setopt($ch, CURLOPT_HTTPHEADER, ['x-access-token: '.$_ENV['ACCESS_TOKEN']]);
+
+            $result = curl_exec ($ch);
+            $decoded = json_decode($result, true);
+            if ($decoded) {
+                $arr = array_merge($arr, $decoded);
+            }
+
+            sleep(1);
+        }
+
+        shuffle($arr);
+
+        $html = '<div>';
+        foreach ($arr as $item) {
+            $html .= $this->replace($item);
+        }
+        $html .= '</div>';
+
+        return $html;
     }
 }
