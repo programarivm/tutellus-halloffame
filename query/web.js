5b96db204da4c40dd2b9ab9b5ba53f8574ec5063
programarivm <info@programarivm.com>
2021-03-04 15:42:43 +0100

    Added query/web.js

---
+++ query/web.js
@@ -0,0 +1,52 @@
+[
+  {
+    "category_code": "tecnologia",
+    "subcategory_code": "bases-de-datos",
+    "q": "mysql"
+  },
+  {
+    "category_code": "tecnologia",
+    "subcategory_code": "desarrollo-web",
+    "q": "laravel"
+  },
+  {
+    "category_code": "tecnologia",
+    "subcategory_code": "desarrollo-web",
+    "q": "symfony"
+  },
+  {
+    "category_code": "tecnologia",
+    "subcategory_code": "desarrollo-web",
+    "q": "docker"
+  },
+  {
+    "category_code": "negocio",
+    "subcategory_code": "seo-y-sem",
+    "q": "seo"
+  },
+  {
+    "category_code": "tecnologia",
+    "subcategory_code": "desarrollo-web",
+    "q": "-divi wordpress"
+  },
+  {
+    "category_code": "tecnologia",
+    "subcategory_code": "software",
+    "q": "debian"
+  },
+  {
+    "category_code": "tecnologia",
+    "subcategory_code": "desarrollo-web",
+    "q": "-svelte -p5js javascript"
+  },
+  {
+    "category_code": "tecnologia",
+    "subcategory_code": "desarrollo-web",
+    "q": "react"
+  },
+  {
+    "category_code": "tecnologia",
+    "subcategory_code": "desarrollo-web",
+    "q": "node"
+  }
+]
