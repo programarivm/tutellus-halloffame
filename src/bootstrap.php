6146dcbaea4ddab864dd597ee53ccf107667740e
programarivm <info@programarivm.com>
2021-03-04 16:54:28 +0100

    Added bootstrap file

---
+++ src/bootstrap.php
@@ -0,0 +1,4 @@
+<?php
+
+define('APP_PATH', realpath(dirname(__FILE__)));
+define('BASE_PATH', realpath(__DIR__.'/../'));
--- src/load.php
+++ src/load.php
@@ -1,5 +1,11 @@
 <?php
 
+require __DIR__.'/bootstrap.php';
+require APP_PATH.'/../vendor/autoload.php';
+
+$dotenv = Dotenv\Dotenv::createImmutable(BASE_PATH);
+$dotenv->load();
+
 $query = json_decode(file_get_contents(__DIR__.'/../query/web.js'));
 $layout = file_get_contents(__DIR__.'/../layout/grav/quark.html');
 
